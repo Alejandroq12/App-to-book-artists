0b6ab750ede1d42a9b1773d2036526215caa7dd7
Julio Quezada <julio.quezada@catolica.edu.sv>
2024-01-24 21:29:07 -0600

    Update models.py

--- models.py
+++ models.py
@@ -1,96 +1,74 @@
-from app import db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+class Genre(db.Model):
+    __tablename__ = 'Genre'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
+venue_genres = db.Table(
+    'VenueGenres', 
+    db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True),
+    db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True)
+)
+
+artist_genres = db.Table(
+    'ArtistGenres', 
+    db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True),
+    db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True)
+)
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
-    def __init__(
-      self, 
-      name,
-      city,
-      state,
-      address,
-      phone,
-      genres,
-      facebook_link,
-      image_link,
-      website,
-      seeking_talent,
-      seeking_description):
-        self.name = name
-        self.city = city
-        self.state = state
-        self.address = address
-        self.phone = phone
-        self.genres = genres
-        self.facebook_link = facebook_link
-        self.image_link = image_link
-        self.website = website
-        self.seeking_talent = seeking_talent
-        self.seeking_description = seeking_description
-    
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120), nullable=False)
-    image_link = db.Column(db.String(500), nullable=False)
-    facebook_link = db.Column(db.String(120), nullable=False)
-    genres = db.Column(db.ARRAY(db.String()), nullable=False)
-    website = db.Column(db.String(120), nullable=False)
-    seeking_talent = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(500), default='')
-    shows = db.relationship('Show', backref='venue', lazy='dynamic')
-    past_shows_count = db.Column(db.Integer)
-    upcoming_shows_count = db.Column(db.Integer)
-    
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    genres = db.relationship('Genre', secondary=venue_genres, backref=db.backref('venues', lazy=True))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='venue', lazy=True) 
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
-    def __init__(
-      self, 
-      name,
-      city,
-      state,
-      phone,
-      genres,
-      facebook_link,
-      image_link,
-      website,
-      seeking_venue,
-      seeking_description):
-        self.name = name
-        self.city = city
-        self.state = state
-        self.phone = phone
-        self.genres = genres
-        self.facebook_link = facebook_link
-        self.image_link = image_link
-        self.website = website
-        self.seeking_venue = seeking_venue
-        self.seeking_description = seeking_description
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    genres = db.relationship('Genre', secondary=artist_genres, backref=db.backref('artists', lazy=True))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='artist', lazy=True) 
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120), nullable=False)
-    genres = db.Column(db.ARRAY(db.String()), nullable=True)
-    image_link = db.Column(db.String(500), nullable=False)
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(500), default='')
-    shows_art = db.relationship('Show', backref='artist', lazy='dynamic')
-    past_shows_count = db.Column(db.Integer)
-    upcoming_shows_count = db.Column(db.Integer)
-
-
-class Show(db.Model):
-  __tablename__ = 'Show'
-
-  id = db.Column(db.Integer, primary_key=True)
-  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-  start_time = db.Column(db.DateTime, nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
