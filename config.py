672264f2426fe1ac5b6f051fa46d88eca36bf621
Julio Quezada <julio.quezada@catolica.edu.sv>
2024-01-24 22:23:23 -0600

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,8 @@ SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-DEBUG = False
+DEBUG = True
 
-# Connect to the database
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'DATABASE_URL'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:4991@localhost:5432/projectOne'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
