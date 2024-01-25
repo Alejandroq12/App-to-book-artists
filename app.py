f906d572d3e846e16668e9ccd0a76e98e9e1703d
Julio Quezada <julio.quezada@catolica.edu.sv>
2024-01-24 20:43:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import json
 import dateutil.parser
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
@@ -586,13 +587,6 @@ if not app.debug:
 
 
 
-# Default port:
-if __name__ == '__main__':
-    app.run()
-
-# Specify port manually:
-'''
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-'''
